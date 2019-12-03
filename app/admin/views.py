f3017f063e5f0561e4e1a3e63c77db4eb7750e26
zhangdashan123 <276333903@qq.com>
2019-12-03 23:05:50 +0800

    v2

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,7 +1,17 @@
 # coding:utf8
+from flask import render_template, redirect
+
 from . import admin
 
 
 @admin.route("/")
 def index():
-    return "<h1 style='color:red'>this is admin</h1>"
+    return render_template("admin/index.html")
+
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+@admin.route("/logout/")
+def logout():
+    return redirect("admin.login")
